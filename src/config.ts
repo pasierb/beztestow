72b8a7c73f1062de3efd0f9783007c285e46830a
Michał Pasierbski <mpasierbski@gmail.com>
2023-07-29 09:50:29 +0000

    Fix twitter profile link

--- src/config.ts
+++ src/config.ts
@@ -4,10 +4,13 @@
 export const SITE_TITLE = '!Bez testów';
 export const SITE_DESCRIPTION =
   'Jak deploy to tylko w piątek. Jak testy to tylko na produkcji.';
-export const TWITTER_HANDLE = '@BezTestow';
 export const MY_NAME = '!Bez testów';
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
 export const SITE_URL = BASE_URL.origin;
 export const DATETIME_LOCALE = 'pl-pl';
+
+// Socials
+export const TWITTER_HANDLE = '@BezTestow';
+export const TWITTER_PROFILE_URL = `https://twitter.com/${TWITTER_HANDLE}`;
