f66af28a145b1690482614871cc6621c000533b4
Michał Pasierbski <mpasierbski@gmail.com>
2023-07-29 13:53:32 +0000

    Add latest article to home

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -2,3 +2,5 @@ import { getCollection } from "astro:content";
 
 export const publishedPosts = await getCollection("blog", ({ data }) => !data.draft)
     .then(posts => posts.sort((a, b) => a.data.date - b.data.date));
+
+export const latestPublishedPost = (await publishedPosts)[0];
